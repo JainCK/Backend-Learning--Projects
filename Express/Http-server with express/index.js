dab39ca5f25784c874f0e2082ec9454ab3337155
Jain K <kuriakosejain456@gmail.com>
2023-12-15 18:55:08 +0530

    added a fucntion

--- Express/Http-server with express/index.js
+++ Express/Http-server with express/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+function sum(n) {
+  let ans = 0;
+  for (let i =0; i <= n ; i++){
+    ans = ans + i;
+  }
+  return ans;
+}
+
 app.get('/', function(req, res){
-  res.send('Hello World!');
+  const n = req.query.n;
+  const ans = sum(n);
+  res.send('Hi your ans is ' + ans);
 });
 
 app.listen(port);
