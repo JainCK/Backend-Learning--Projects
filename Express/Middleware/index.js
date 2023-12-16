46290b038f16fa91f0d28d15e1fae6eac00b1cab
Jain K <kuriakosejain456@gmail.com>
2023-12-16 20:03:27 +0530

    learning middleware

--- Express/Middleware/index.js
+++ Express/Middleware/index.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const app = express();
 
-app.get('/health-check', (req, res) => {
+//app.get('/health-check', (req, res) => {
 
-    const username = req.headers.username;
-    const password = req.headers.password;
-    const kidneyId = req.headers.kidneyId;
+//const username = req.headers.username;
+//const password = req.headers.password;
+//const kidneyId = req.headers.kidneyId;
+//
+//    if(username === "jain" && password === "pass") {
+//        res.status(400).json({"msg": "Somethings up with your ionputs"})
+//        return
+//    }
+//
+//    if (kidneyId != 1 && kidneyId != 2) {
+//        res.status(400).json({"msg": "Somethings up with your ionputs"})
+//        return
+//    }
+//
+//    res.json({
+//        msg: "Your kidney is fine!"
+//    })
+//
+//});
 
-    if(username === "jain" && password === "pass") {
-        res.status(400).json({"msg": "Somethings up with your ionputs"})
-        return
-    }
+app.use(express.json());
 
-    if (kidneyId != 1 && kidneyId != 2) {
-        res.status(400).json({"msg": "Somethings up with your ionputs"})
-        return
-    }
+app.post('/health-checkup', (req, res) => {
+    const kid = req.body.kid;
+    const kidLength = kid.length;
 
-    res.json({
-        msg: "Your kidney is fine!"
-    })
+    res.send ("you have" + kidLength + "kids");
+})
 
-});
 
 app.listen(3000);
