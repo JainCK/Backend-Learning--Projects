5a401716277c5b5de718b1f4dfff4c9f029cd91f
Jain K <kuriakosejain456@gmail.com>
2023-12-17 19:46:54 +0530

    solution added

--- Cert-EM-projects/Timestamp-MS/index.js
+++ Cert-EM-projects/Timestamp-MS/index.js
@@ -1,19 +1,14 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -24,6 +19,28 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
+});
+
+app.get("/api/:date", (req, res) => {
+  let rawDate = new Date(req.params.date);
+
+  if (rawDate.toString() === "Invalid Date") {
+    rawDate = new Date(parseInt(req.params.date));
+  }
+
+  if (isNaN(rawDate)) {
+    return res.status(400).json({ error: "Invalid Date" });
+  }
+  return res.status(200).json({
+    unix: rawDate.getTime(),
+    utc: rawDate.toUTCString(),
+  });
+});
 
 
 // listen for requests :)
